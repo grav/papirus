7d7749d5fe16e67d5a33adc3c8dd1df0d9b35a52
Mikkel Gravgaard <grav@meshell.local>
2011-04-14 18:17:02 +0200

    works for information.dk (articles and telegrams)

--- Rakefile
+++ Rakefile
@@ -3,5 +3,9 @@ task :bookmarklet do
   s = IO.read("bookmarklet.js")
   s.gsub!(/\/\*\*.*\*\//m,"")
   s.delete!("\n")
-  puts "javascript:"+(URI.escape s)
+  url = "javascript:"+(URI.escape s)
+  html = "<html><p><a href=\"#{url}\">Papirus</a> &lt;-- drag to bookmark Bar</p>  <p><textarea>#{url}</textarea></p></html>"
+
+  File.open("papirus.html","w") { |f| f << html}
+
 end
--- papirus.js
+++ papirus.js
@@ -17,43 +17,83 @@ function includeScript(scriptName) {
 }
 
 // TODO: include only if not already included
-//includeScript("jquery-1.5.2.js");
+// or include as something else, so that we have newest version
+includeScript("jquery-1.5.2.js");
 
 function showOnly(elm){
-    $(elm).show();
-    $(elm).siblings().hide();
+    jqPap(elm).show();
+    jqPap(elm).siblings().hide();
     if (elm!=null && elm!=undefined) showOnly(elm.parentElement);
 }
 
 function show(elm){
-    try{
-        $(elm).show();
-
-    } catch(e){
-        console.log(e)
-    }
-    if (elm!=null) show(elm.parentElement);
+        if(elm!=null){
+            console.log("elm: "+elm.id+"\n");
+            jqPap(elm).show();
+            if (elm.parentElement!=undefined){
+                show(elm.parentElement);
+            }
+        }
 }
 
-//$("body").hide();
+function hideAll(){
+    jqPap("*").hide();
+}
 
-//show($("#page-content")[0]);
 
-//if (window.location.href.match(/^http\:\/\/www\.information\.dk\/\d+/)){
-//    information();
-//}
-//
-//
-//
-//
-//
-//
-//function information(){
-//    $(document.body).hide()
-//
-//
-//
-//
-//    $("#page-content").show();
-//
-//}
+function show(elm){
+    jqPap(elm).parentsUntil().show();
+    jqPap(elm).show();
+    jqPap(elm+" *").show();
+}
+
+function scale(sel,scale){
+    console.log("scaling " + sel + " to "+scale);
+    jqPap(sel).width(jqPap(sel).width()*scale);
+    jqPap(sel).height(jqPap(sel).height()*scale);
+}
+
+var jqPap;
+function inf(){
+
+    jqPap = jQuery.noConflict();
+
+    hideAll();
+
+    show(".title");
+    show(".col.col-five");
+    var width;
+    if(jqPap(".article-body").length>0){
+        // image may be wider than .article-body
+        width = jqPap(".article-body").width();
+    } else {
+        // telegrams do not .have article-body
+        width = jqPap("#telegram-wrapper").width();
+    }
+    jqPap(".webteaser").width(width);
+    jqPap(".col.col-five").width(width);
+
+    var imgDiv = ".image-box-large"
+    if(jqPap(imgDiv).length>0){
+        // Do some image scaling
+        var oldCaptionHeight = jqPap(".caption").height();
+        var scale = jqPap(".article-body").width() / jqPap(imgDiv).width();
+        scale(imgDiv,scale);
+        scale(imgDiv+" img",scale);
+
+        // move text further down
+        var offset = jqPap(".col.col-three").offset();
+        // seems that we need some extra space. And height difference is not enough?
+        var extraHeight = 10;
+        offset.top += jqPap(".caption").height() - oldCaptionHeight + extraHeight;
+        jqPap(".col.col-three").offset(offset);
+    }
+    jqPap("#page").width(width);
+    // we make <body> smaller since everything is centered in it
+    var margin = 10; //preserve some margin
+    jqPap("body").width(width+2*margin);
+}
+
+
+// delay execution to allow loading of jquery
+setTimeout(inf,500);
